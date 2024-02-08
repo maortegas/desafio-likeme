a22cd9865c30a28bc0b2db66a658e3f26998a04d
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2024-02-08 15:33:45 -0300

    Add: Create a post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,6 @@ function App() {
   // este método se utilizará en el siguiente desafío
   const like = async (id) => {
     await axios.put(urlBaseServer + `/posts/like/${id}`);
-    console.log(id);
     getPosts();
   };
 
